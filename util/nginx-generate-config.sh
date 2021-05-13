9180969d58336b2a25de59dac0209f6210dd5925
Komphet <komphet.me@gmail.com>
2021-05-13 20:33:39 +0700

    Update Host

--- util/nginx-generate-config.sh
+++ util/nginx-generate-config.sh
@@ -17,7 +17,7 @@ server {
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
 	    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-	    proxy_set_header X-Forwarded-Host \$host:\$server_port;
+	    proxy_set_header X-Forwarded-Host \$host;
         proxy_set_header X-Forwarded-Server \$host;
         proxy_pass http://0.0.0.0:$PORT;
     }
