51b26e04b0be0343036aa608bfbce0e2c924fb85
Komphet <komphet.me@gmail.com>
2021-05-14 08:43:31 +0700

    Update generate cert

--- install/ubuntu/jenkins.sh
+++ install/ubuntu/jenkins.sh
@@ -118,7 +118,6 @@ if [ -n "$DOMAIN" ]; then
 		--server-name=$DOMAIN \
 		--port=8080"
 
-	bash -c "$(wget -q -O - https://raw.githubusercontent.com/lattesoft/devops-script/main/util/certbot-generate-cert.sh)" '' --domain=$DOMAIN
 	if [ -n "$CLOUDFLARE_ZONE" ] && [ -n "$CLOUDFLARE_TOKEN" ] && [ -n "$CLOUDFLARE_DNS_TYPE" ] && [ -n "$CLOUDFLARE_DNS_CONTENT" ] ; then
 		echo "Cloudflare config is not empty."
 		sudo sh -c "bash -c '$(wget -q -O - https://raw.githubusercontent.com/lattesoft/devops-script/main/util/cloudflare-update-dns.sh)' '' \
@@ -129,6 +128,7 @@ if [ -n "$DOMAIN" ]; then
 			--cloudflare-dns-content=$CLOUDFLARE_DNS_CONTENT \
 			--cloudflare-dns-ttl=120 \
 			--cloudflare-dns-proxied=false"
+    bash -c "$(wget -q -O - https://raw.githubusercontent.com/lattesoft/devops-script/main/util/certbot-generate-cert.sh)" '' --domain=$DOMAIN
 	fi
 fi
 
